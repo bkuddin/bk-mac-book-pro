f4cf125331087376e022ee096a56a633beadc297
BK Uddin <1bkhossain@gmail.com>
2021-08-21 20:10:20 +0600

    Update Js

--- js/script.js
+++ js/script.js
@@ -4,16 +4,17 @@ function updatePrice() {
   const plusStorageCost = Number(exStorageCost.innerText);
   const plusDeleveryCost = Number(deleveryCost.innerText);
   const newBestPrice = Number(bestPrice.innerText);
-  const grandTotal =
+
+  const newTotal =
     newBestPrice + plusMemoryCost + plusStorageCost + plusDeleveryCost;
-  totalPrice.innerText = grandTotal;
+  totalPrice.innerText = Number(totalPrice.innerText);
+  totalPrice.innerText = newTotal;
 }
 
 /* Memory */
 
 const eightGb = document.getElementById("config-memory-eight-gb");
 const sixteenGb = document.getElementById("config-memory-sixteen-gb");
-
 const exMemoryCost = document.getElementById("ex-memory-cost");
 
 // Add Event Listerner of Memories
@@ -25,7 +26,6 @@ eightGb.addEventListener("click", function () {
 
 sixteenGb.addEventListener("click", function () {
   exMemoryCost.innerText = 180;
-
   updatePrice();
 });
 
@@ -34,7 +34,6 @@ sixteenGb.addEventListener("click", function () {
 const ssdFiveTwelveGb = document.getElementById("config-ssd-fiveTwelve-gb");
 const ssdOneTb = document.getElementById("config-ssd-one-tb");
 const ssdTwoFiftySixGb = document.getElementById("config-ssd-twoFiftySix-gb");
-
 const exStorageCost = document.getElementById("ex-storage-cost");
 
 // Add Event Listerner of Storeges
@@ -58,7 +57,6 @@ ssdOneTb.addEventListener("click", function () {
 
 const freeDelevery = document.getElementById("free-delevery");
 const chargeDelevery = document.getElementById("charge-delevery");
-
 const deleveryCost = document.getElementById("delevery-cost");
 
 // Add Event Listerner of Storeges Delelveries
@@ -77,10 +75,18 @@ chargeDelevery.addEventListener("click", function () {
 // best price
 
 const bestPrice = document.getElementById("best-price");
-bestPrice.addEventListener("click", function () {
+bestPrice.innerText = 1299;
+/* bestPrice.addEventListener("click", function () {
   bestPrice.innerText = 1299;
-});
+}); */
 
 /* Total Price */
 
 const totalPrice = document.getElementById("total");
+
+// Bonus Part-----------------------------------
+
+// promo code
+const grandTotal = document.getElementById("grand-Total");
+const promoInput = document.getElementById("promo-input");
+const promoBtn = document.getElementById("promo-btn");
